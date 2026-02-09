2ddfcc7b4e9c3d86c7aba41d0da98634c1b6cbd8
yuri <yurigx12345@gmail.com>
2026-02-09 16:32:01 -0300

    🐛 FIX: Adição de nova rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard'; 
 import { FormComponent } from './pages/form/form.component';
-
+import { TelaInicialComponent } from './pages/tela-inicial/tela-inicial.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   //{ path: 'login', component: LoginComponent },
@@ -11,8 +11,8 @@ export const routes: Routes = [
   //{ path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard] },
 
    { path: 'form', component: FormComponent},
-
+   {path: 'tela-inicial', component: TelaInicialComponent},
 
   // Rota coringa para redirecionar caso o usuário tente acessar uma página inexistente
-  { path: '**', redirectTo: 'form' }
+  { path: '**', redirectTo: 'tela-inicial' }
 ];
