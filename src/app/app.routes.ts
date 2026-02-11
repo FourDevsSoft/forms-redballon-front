83bc80e14386d248bca4febe162534b7ef40dd69
yuri <yurigx12345@gmail.com>
2026-02-11 16:38:55 -0300

    🐛 FIX:  Novas rotas de paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard'; 
 import { FormComponent } from './pages/form/form.component';
 import { TelaInicialComponent } from './pages/tela-inicial/tela-inicial.component';
+import { NovoResponsavelComponent } from './pages/novo-responsavel/novo-responsavel.component';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   //{ path: 'login', component: LoginComponent },
@@ -9,7 +11,7 @@ export const routes: Routes = [
   //DIRETOR ACESSA TUDO
   // Rotas protegidas pelo AuthGuard
   //{ path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard] },
-
+   { path: 'novo-responsavel', component: NovoResponsavelComponent},
    { path: 'form', component: FormComponent},
    {path: 'tela-inicial', component: TelaInicialComponent},
 
